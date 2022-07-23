f8d4561faa5531e82fc71235b1853f9d66b2c8f5
Mohammed Yasser <mohammedre4a@gmail.com>
2022-07-23 23:26:20 +0200

    remove: remove lines for general/generic InputField and will separate them into different classes

--- lib/reusable_components/InputFieldWithLabel.dart
+++ lib/reusable_components/InputFieldWithLabel.dart
@@ -3,16 +3,8 @@ import 'package:flutter/material.dart';
 class InputFieldWithLabel extends StatelessWidget {
   String label, hint;
   int width;
-  IconData? endIcon;
-  List<String>? items;
-
   InputFieldWithLabel(
-      {Key? key,
-      required this.label,
-      required this.hint,
-      required this.width,
-      this.endIcon,
-      this.items})
+      {Key? key, required this.label, required this.hint, required this.width})
       : super(key: key);
 
   @override
@@ -21,38 +13,28 @@ class InputFieldWithLabel extends StatelessWidget {
       color: Colors.white,
       padding: const EdgeInsets.all(16),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            label,
-            style: const TextStyle(
-                color: Colors.black87,
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
-                decoration: TextDecoration.none),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
-            child: TextFormField(
-              decoration: InputDecoration(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              label,
+              style: const TextStyle(
+                  color: Colors.black87,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.none),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
+              child: TextFormField(
+                decoration: InputDecoration(
                   labelText: hint,
                   fillColor: Colors.black12,
                   filled: true,
-                  suffixIcon: Padding(
-                    padding: const EdgeInsets.only(right: 15, left: 15),
-                    //handling icons for pickers and lists
-                    child: items != null && endIcon == null
-                        ? const Icon(Icons.keyboard_arrow_down)
-                        : endIcon != null
-                            ? Icon(endIcon)
-                            : null,
-                  )),
+                ),
+              ),
             ),
-          ),
-        ],
-      ),
+          ]),
     );
   }
 }
-//todo: separate Date picker and list from this class
