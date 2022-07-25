c91121f9e644e5488ded757408a24f3e222850e8
Mohammed Yasser <mohammedre4a@gmail.com>
2022-07-25 16:33:30 +0200

    refactor: separate home screen code from myApp (solve navigation problem), added Tab bar & actions' icons for home page to navigate between different tasks.

--- lib/Screens/main.dart
+++ lib/Screens/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:plan_em/Screens/TaskCreationScreen.dart';
+import 'package:plan_em/Screens/Tasks/AllTasks.dart';
+import 'package:plan_em/Screens/Tasks/CompletedTasks.dart';
+import 'package:plan_em/Screens/Tasks/FavoriteTasks.dart';
+import 'package:plan_em/Screens/Tasks/UncompletedTasks.dart';
 import 'package:plan_em/reusable_components/PrimaryButton.dart';
 
 void main() {
@@ -13,41 +17,124 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/MainScreen',
-        routes: {
-        '/MainScreen': (context) => const MyApp(),
-          '/TaskCreationScreen': (context) => const TaskCreationScreen(),
+      initialRoute: HomePage.routeName,
+      routes: {
+        HomePage.routeName: (context) => const HomePage(),
+        TaskCreationScreen.routeName: (context) => const TaskCreationScreen(),
+      },
+      title: "Plan 'em",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color.fromARGB(255, 17, 17, 17),
+        backgroundColor: Colors.white,
+        accentColor: Colors.teal,
+        textTheme: TextTheme(
+            headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+            headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+            bodyText2: TextStyle(fontSize: 11.0, fontFamily: 'Hind'),
+            bodyText1: TextStyle(fontSize: 14.0, fontFamily: 'Hind')),
+        appBarTheme: AppBarTheme(
+            backgroundColor: Theme.of(context).backgroundColor,
+            foregroundColor: Theme.of(context).primaryColor,
+            iconTheme: IconThemeData(
+              color: Theme.of(context).primaryColor,
+            )),
+        buttonColor: Colors.teal,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
 
-        },
-        title: "Plan 'em",
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.green,
+class HomePage extends StatelessWidget {
+  static String routeName = '/HomeScreen';
+
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      initialIndex: 0,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          //define tab bar header
+          bottom: TabBar(
+            indicatorColor: Colors.green,
+            tabs: [
+              Tab(
+                  child: Text(
+                "All",
+                style: Theme.of(context).textTheme.bodyText2,
+              )),
+              Tab(
+                  child: Text("Completed",
+                      style: Theme.of(context).textTheme.bodyText2)),
+              Tab(
+                  child: Text("Uncompleted",
+                      style: Theme.of(context).textTheme.bodyText2)),
+              Tab(
+                  child: Text("Favorite",
+                      style: Theme.of(context).textTheme.bodyText2)),
+            ],
+          ),
+          actions: const [
+            //todo: handle clicks
+            Padding(
+              padding: EdgeInsets.only(left: 7, right: 7),
+              child: Icon(
+                Icons.search,
+                size: 28,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 7, right: 7),
+              child: Icon(
+                Icons.notifications_none_outlined,
+                size: 28,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 7, right: 7),
+              child: Icon(
+                Icons.menu,
+                size: 28,
+              ),
+            ),
+          ],
+          title: Text(
+            "Board",
+            style: TextStyle(
+                fontSize: 25,
+                fontStyle: FontStyle.normal,
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).primaryColor),
+          ),
+          backgroundColor: Theme.of(context).backgroundColor,
+          automaticallyImplyLeading: false,
         ),
-        home: Scaffold(
-          backgroundColor: Colors.white,
-          appBar: AppBar(
-            actions: const [
-              //todo: handle clicks
-              Icon(Icons.search),
-              Icon(Icons.notifications_none_outlined),
-              Icon(Icons.menu)
-            ],
-            title: const Text("Board",style: const TextStyle(fontSize: 18,fontWeight: FontWeight.bold)),
-            backgroundColor: Colors.white,
-          ),
-          body:
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              //todo: create tabBar
-              //todo: create listTiles to contain lists
-              PrimaryButton(label: "Add a Task", onClickFunction: (){
-                Navigator.pushNamed(context, '/TaskCreationScreen');
-              })
+              //define different tabs/screens
+              const TabBarView(children: [
+                AllTasks(),
+                CompletedTasks(),
+                UncompletedTasks(),
+                FavoriteTasks()
+              ]),
+              //todo: add button to task screens.
+              // PrimaryButton(
+              //     label: "Add a Task",
+              //     onClickFunction: () {
+              //       Navigator.pushNamed(context, TaskCreationScreen.routeName);
+              //     })
             ],
           ),
-        )
+        ),
+      ),
     );
   }
 }
