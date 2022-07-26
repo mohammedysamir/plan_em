e5690c24e20730b28410ef5026ff226578508ff8
Mohammed Yasser <mohammedre4a@gmail.com>
2022-07-26 21:50:11 +0200

    refactor: transform DateTimeSelector to widget.

--- lib/reusable_components/DateTimeSelector.dart
+++ lib/reusable_components/DateTimeSelector.dart
@@ -1,88 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class DateTimeSelector extends StatefulWidget {
-  final String label, hint;
-  final int width;
-  final bool isDatePicker;
-
-  const DateTimeSelector(
-      {Key? key,
-      required this.label,
-      required this.hint,
-      required this.width,
-      required this.isDatePicker})
-      : super(key: key);
-
-  @override
-  State<DateTimeSelector> createState() => _DateTimeSelectorState();
-}
-
-class _DateTimeSelectorState extends State<DateTimeSelector> {
-  TextEditingController controller = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      width: widget.width.toDouble(),
-      padding: const EdgeInsets.all(16),
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              widget.label,
-              style: const TextStyle(
-                  color: Colors.black87,
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                  decoration: TextDecoration.none),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
-              child: TextFormField(
-                controller: controller,
-                //manipulate icons
-                decoration: InputDecoration(
-                    labelText: widget.hint,
-                    fillColor: Colors.black12,
-                    filled: true,
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Theme.of(context).accentColor
-                        )
-                    ),
-                    suffixIcon: widget.isDatePicker
-                        ? const Icon(Icons.date_range_outlined)
-                        : const Icon(Icons.timer_outlined)),
-                onTap: () async {
-                  //suite time or date picker
-                  DateTime? datePicker;
-                  TimeOfDay? timePicker;
-                  if (widget.isDatePicker) {
-                    datePicker = await showDatePicker(
-                        context: context,
-                        initialDate: DateTime.now(),
-                        firstDate: DateTime(DateTime.now().year),
-                        lastDate: DateTime(2200));
-                  } else {
-                    timePicker = await showTimePicker(
-                        context: context,
-                        initialTime: const TimeOfDay(hour: 11, minute: 0));
-                  }
-                  setState(() {
-                    controller.text = datePicker != null
-                        ? DateFormat('yyyy-MM-dd').format(datePicker)
-                        : timePicker != null
-                            ? timePicker.format(context)
-                            : "";
-                  });
-                },
-              ),
-            ),
-          ]),
-    );
-  }
-}
+Widget DateTimeSelector({
+  required String label,hint,
+  required int width,
+  required bool isDatePicker,
+  required BuildContext context,
+  required TextEditingController controller
+}) => Container(
+  color: Colors.white,
+  width: width.toDouble(),
+  padding: const EdgeInsets.all(16),
+  child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: const TextStyle(
+              color: Colors.black87,
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+              decoration: TextDecoration.none),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
+          child: TextFormField(
+            controller: controller,
+            //manipulate icons
+            decoration: InputDecoration(
+                labelText:hint,
+                fillColor: Colors.black12,
+                filled: true,
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Theme.of(context).accentColor
+                    )
+                ),
+                suffixIcon: isDatePicker
+                    ? const Icon(Icons.date_range_outlined)
+                    : const Icon(Icons.timer_outlined)),
+            onTap: () async {
+              //suite time or date picker
+              DateTime? datePicker;
+              TimeOfDay? timePicker;
+              if (isDatePicker) {
+                datePicker = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(DateTime.now().year),
+                    lastDate: DateTime(2200));
+              } else {
+                timePicker = await showTimePicker(
+                    context: context,
+                    initialTime: const TimeOfDay(hour: 11, minute: 0));
+              }
+              // setState(() {
+              //   controller.text = datePicker != null
+              //       ? DateFormat('yyyy-MM-dd').format(datePicker)
+              //       : timePicker != null
+              //       ? timePicker.format(context)
+              //       : "";
+              // });
+            },
+          ),
+        ),
+      ]),
+);
 //todo: embed Bloc state management instead of setState()
