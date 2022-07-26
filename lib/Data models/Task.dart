68c1367059562f5c512339de6d6af06a9388f4cf
Mohammed Yasser <mohammedre4a@gmail.com>
2022-07-26 22:02:46 +0200

    refactor: added list for reminders.

--- lib/Data models/Task.dart
+++ lib/Data models/Task.dart
@@ -3,19 +3,30 @@ import 'package:flutter/material.dart';
 class Task {
   final String taskLabel;
   final Repetition? repetition;
-
-  //todo: add reminder
-  final TimeOfDay? startTime, endTime;
-  final DateTime? deadline;
+  final String? reminder,startTime,endTime,deadline;
+  final bool isFavorite, isComplete;
 
   const Task(
       {required this.taskLabel,
+      required this.isFavorite,
+      required this.isComplete,
       this.repetition,
       this.startTime,
       this.endTime,
-      this.deadline});
+      this.deadline,
+      this.reminder});
 }
+
 /*
 * Enumeration to make the repetition discrete not a string.
 * */
 enum Repetition { daily, weekly, monthly, yearly }
+
+const List Reminder = [
+  "10 minutes early",
+  "30 minutes early",
+  "1 hour early",
+  "1 day early",
+  "1 week before",
+  "1 month before"
+];
--- lib/reusable_components/DateTimeSelector.dart
+++ lib/reusable_components/DateTimeSelector.dart
@@ -1,70 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
-Widget DateTimeSelector({
-  required String label,hint,
-  required int width,
-  required bool isDatePicker,
-  required BuildContext context,
-  required TextEditingController controller
-}) => Container(
-  color: Colors.white,
-  width: width.toDouble(),
-  padding: const EdgeInsets.all(16),
-  child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          label,
-          style: const TextStyle(
-              color: Colors.black87,
-              fontSize: 15,
-              fontWeight: FontWeight.bold,
-              decoration: TextDecoration.none),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
-          child: TextFormField(
-            controller: controller,
-            //manipulate icons
-            decoration: InputDecoration(
-                labelText:hint,
-                fillColor: Colors.black12,
-                filled: true,
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                        color: Theme.of(context).accentColor
-                    )
-                ),
-                suffixIcon: isDatePicker
-                    ? const Icon(Icons.date_range_outlined)
-                    : const Icon(Icons.timer_outlined)),
-            onTap: () async {
-              //suite time or date picker
-              DateTime? datePicker;
-              TimeOfDay? timePicker;
-              if (isDatePicker) {
-                datePicker = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(DateTime.now().year),
-                    lastDate: DateTime(2200));
-              } else {
-                timePicker = await showTimePicker(
-                    context: context,
-                    initialTime: const TimeOfDay(hour: 11, minute: 0));
-              }
-              // setState(() {
-              //   controller.text = datePicker != null
-              //       ? DateFormat('yyyy-MM-dd').format(datePicker)
-              //       : timePicker != null
-              //       ? timePicker.format(context)
-              //       : "";
-              // });
-            },
-          ),
-        ),
-      ]),
-);
+Widget DateTimeSelector(
+        {required String label,
+        hint,
+        required int width,
+        required bool isDatePicker,
+        required BuildContext context,
+        required TextEditingController controller}) =>
+    Container(
+      color: Colors.white,
+      width: width.toDouble(),
+      padding: const EdgeInsets.all(16),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              label,
+              style: const TextStyle(
+                  color: Colors.black87,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.none),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
+              child: TextFormField(
+                controller: controller,
+                //manipulate icons
+                decoration: InputDecoration(
+                    labelText: hint,
+                    fillColor: Colors.black12,
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Theme.of(context).accentColor)),
+                    suffixIcon: isDatePicker
+                        ? const Icon(Icons.date_range_outlined)
+                        : const Icon(Icons.timer_outlined)),
+                onTap: () async {
+                  //suite time or date picker
+                  DateTime? datePicker;
+                  TimeOfDay? timePicker;
+                  if (isDatePicker) {
+                    datePicker = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(DateTime.now().year),
+                        lastDate: DateTime(2200));
+                  } else {
+                    timePicker = await showTimePicker(
+                        context: context,
+                        initialTime: const TimeOfDay(hour: 11, minute: 0));
+                  }
+                  // setState(() {
+                  //   controller.text = datePicker != null
+                  //       ? DateFormat('yyyy-MM-dd').format(datePicker)
+                  //       : timePicker != null
+                  //       ? timePicker.format(context)
+                  //       : "";
+                  // });
+                },
+              ),
+            ),
+          ]),
+    );
 //todo: embed Bloc state management instead of setState()
