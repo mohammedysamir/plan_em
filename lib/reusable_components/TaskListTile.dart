1f2af2ef38a44084c07ac069e88ca08d4b83e58c
Mohammed Yasser <mohammedre4a@gmail.com>
2022-07-27 00:05:22 +0200

    refactor: re-assign colors for taskTile. feat: added initial functions for manipulating task completeness and favorite.

--- lib/reusable_components/TaskListTile.dart
+++ lib/reusable_components/TaskListTile.dart
@@ -1,14 +1,19 @@
-import 'package:flutter/material.dart';
 import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:plan_em/TaskDatabase.dart';
+
+import 'Constants.dart';
 
 class TaskListTile extends StatefulWidget {
   String taskLabel;
   bool isFavorite, isComplete;
 
-  TaskListTile({Key? key,
-    required this.taskLabel,
-    required this.isComplete,
-    required this.isFavorite})
+  TaskListTile(
+      {Key? key,
+      required this.taskLabel,
+      required this.isComplete,
+      required this.isFavorite})
       : super(key: key);
 
   @override
@@ -18,47 +23,45 @@ class TaskListTile extends StatefulWidget {
 class _TaskListTileState extends State<TaskListTile> {
   @override
   Widget build(BuildContext context) {
-    Random random=new Random();
+    Random random = new Random();
     return ListTile(
-      leading: Checkbox(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
-          value: widget.isComplete,
-          fillColor: MaterialStateProperty.all<Color>(Color.fromRGBO(
-            random.nextInt(255)+100,
-            random.nextInt(255)+100,
-            random.nextInt(255)+100,
-            1,
-          )),
-          onChanged: (change) {
-            setState(() {
-              widget.isComplete = change!;
-              //todo: edit completeness in db
-            });
-          }),
-      title: Text(
-        widget.taskLabel,
-        style: TextStyle(
-            color: Theme
-                .of(context)
-                .primaryColor,
-            fontSize: 18,
-            decoration: widget.isComplete
-                ? TextDecoration.lineThrough
-                : TextDecoration.none),
-      ),
-      trailing: GestureDetector(
-        onTap: () {
-          //add function to mark task as favorite
-          setState((){
-            widget.isFavorite = !widget.isFavorite;
-            //todo: complete this function with  db
-          });
-        },
-        child: Icon(
-          widget.isFavorite ? Icons.star : Icons.star_border_outlined,
-          color: Colors.yellow,
+        leading: Checkbox(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(5))),
+            value: widget.isComplete,
+            fillColor: MaterialStateProperty.all<Color>(
+                colors[random.nextInt(colors.length)]),
+            onChanged: (change) {
+              setState(() {
+                widget.isComplete = change!;
+                TaskDatabase.instance
+                    .updateCompletedTask(widget.taskLabel, widget.isComplete);
+                //todo: test update function
+              });
+            }),
+        title: Text(
+          widget.taskLabel,
+          style: TextStyle(
+              color: Theme.of(context).primaryColor,
+              fontSize: 18,
+              decoration: widget.isComplete
+                  ? TextDecoration.lineThrough
+                  : TextDecoration.none),
         ),
-      )
-    );
+        trailing: GestureDetector(
+          onTap: () {
+            //add function to mark task as favorite
+            setState(() {
+              widget.isFavorite = !widget.isFavorite;
+              TaskDatabase.instance
+                  .updateFavoriteTask(widget.taskLabel, widget.isFavorite);
+              //todo: test update function
+            });
+          },
+          child: Icon(
+            widget.isFavorite ? Icons.star : Icons.star_border_outlined,
+            color: Colors.yellow,
+          ),
+        ));
   }
 }
