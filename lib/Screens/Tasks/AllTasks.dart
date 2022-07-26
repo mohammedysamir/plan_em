25fdc08ceb5df99c962d1ccf54d60158fb62ba0a
Mohammed Yasser <mohammedre4a@gmail.com>
2022-07-26 22:17:42 +0200

    refactor: recreate dummy data of tasks.

--- lib/Screens/Tasks/AllTasks.dart
+++ lib/Screens/Tasks/AllTasks.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:plan_em/reusable_components/TaskListTile.dart';
-import 'package:plan_em/reusable_components/PrimaryButton.dart';
+
 import '../../Data models/Task.dart';
-import '../TaskCreationScreen.dart';
+import '../../reusable_components/Constants.dart';
+
 //todo: create list of listTiles to be viewed and fetched from DB
-List<Task> tasks=<Task>[
-  Task(taskLabel: "this is task 1",repetition:Repetition.daily,startTime: TimeOfDay.now(),endTime: TimeOfDay.now(),deadline: DateTime.now()),
-  Task(taskLabel: "this is task 2",repetition:Repetition.daily,startTime: TimeOfDay.now(),endTime: TimeOfDay.now(),deadline: DateTime.now()),
-  Task(taskLabel: "this is task 3",repetition:Repetition.daily,startTime: TimeOfDay.now(),endTime: TimeOfDay.now(),deadline: DateTime.now()),
+List<Task> tasks = <Task>[
+  Task(
+      taskLabel: "this is task 2",
+      repetition: Repetition.daily,
+      startTime: TimeOfDay.now().toString(),
+      endTime: TimeOfDay.now().toString(),
+      deadline: DateTime.now().toString(),
+      isComplete: false,
+      isFavorite: true),
+  Task(
+      taskLabel: "this is task 1",
+      repetition: Repetition.daily,
+      startTime: TimeOfDay.now().toString(),
+      endTime: TimeOfDay.now().toString(),
+      deadline: DateTime.now().toString(),
+      isComplete: true,
+      isFavorite: false),
+  Task(
+      taskLabel: "this is task 3",
+      repetition: Repetition.daily,
+      startTime: TimeOfDay.now().toString(),
+      endTime: TimeOfDay.now().toString(),
+      deadline: DateTime.now().toString(),
+      isComplete: false,
+      isFavorite: false),
 ];
+
 class AllTasks extends StatefulWidget {
   const AllTasks({Key? key}) : super(key: key);
 
@@ -20,14 +43,16 @@ class _AllTasksState extends State<AllTasks> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.height*0.75,
+      height: MediaQuery.of(context).size.height * 0.75,
       child: ListView.builder(
           //to be assigned to fetched list.size
           itemCount: tasks.length,
-          itemBuilder:(BuildContext context,int index){
-            return  TaskListTile(taskLabel: tasks[index].taskLabel, isComplete: true, isFavorite: false);
-          }
-      ),
+          itemBuilder: (BuildContext context, int index) {
+            return TaskListTile(
+                taskLabel: tasks[index].taskLabel,
+                isComplete: tasks[index].isComplete,
+                isFavorite: tasks[index].isFavorite);
+          }),
     );
   }
 }
