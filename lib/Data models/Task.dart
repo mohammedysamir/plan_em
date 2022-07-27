38147b36ba59a4c59f3bc31f304e478a7a20da48
Mohammed Yasser <mohammedre4a@gmail.com>
2022-07-27 04:05:10 +0200

    feat: added function to map Tasks.

--- lib/Data models/Task.dart
+++ lib/Data models/Task.dart
@@ -14,4 +14,18 @@ class Task {
       this.endTime,
       this.deadline,
       this.reminder});
+
+  Task.fromMap(Map<String, dynamic> res)
+      : taskLabel = res["label"],
+        isFavorite = res["isFavorite"],
+        isComplete = res["isComplete"],
+        repetition = res["repetition"],
+        startTime = res["startTime"],
+        endTime = res["endTime"],
+        deadline = res["deadline"],
+        reminder = res["reminder"];
+
+  Map<String, Object?> toMap() {
+    return {'label':taskLabel,'isFavorite': isFavorite, 'isComplete': isComplete, 'repetition': repetition, 'startTime': startTime, 'endTime':endTime,'deadline':deadline, 'reminder':reminder};
+  }
 }
