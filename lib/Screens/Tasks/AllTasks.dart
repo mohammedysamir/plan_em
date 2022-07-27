429ba9ef35c88a3f3900c1de29fb895040bce827
Mohammed Yasser <mohammedre4a@gmail.com>
2022-07-27 18:54:47 +0200

    refactor: added FutureBuilder to fetch db data.

--- lib/Screens/Tasks/AllTasks.dart
+++ lib/Screens/Tasks/AllTasks.dart
@@ -1,36 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:plan_em/TaskDatabase.dart';
 import 'package:plan_em/reusable_components/TaskListTile.dart';
 
 import '../../Data models/Task.dart';
-import '../../reusable_components/Constants.dart';
 
 //todo: create list of listTiles to be viewed and fetched from DB
-List<Task> tasks = <Task>[
-  Task(
-      taskLabel: "this is task 2",
-      repetition: Repetition[1],
-      startTime: TimeOfDay.now().toString(),
-      endTime: TimeOfDay.now().toString(),
-      deadline: DateTime.now().toString(),
-      isComplete: false,
-      isFavorite: true),
-  Task(
-      taskLabel: "this is task 1",
-      repetition: Repetition[0],
-      startTime: TimeOfDay.now().toString(),
-      endTime: TimeOfDay.now().toString(),
-      deadline: DateTime.now().toString(),
-      isComplete: true,
-      isFavorite: false),
-  Task(
-      taskLabel: "this is task 3",
-      repetition: Repetition[0],
-      startTime: TimeOfDay.now().toString(),
-      endTime: TimeOfDay.now().toString(),
-      deadline: DateTime.now().toString(),
-      isComplete: false,
-      isFavorite: false),
-];
+// List<Task> tasks = <Task>[
+//   Task(
+//       taskLabel: "this is task 2",
+//       repetition: Repetition[1],
+//       startTime: TimeOfDay.now().toString(),
+//       endTime: TimeOfDay.now().toString(),
+//       deadline: DateTime.now().toString(),
+//       isComplete: false,
+//       isFavorite: true),
+//   Task(
+//       taskLabel: "this is task 1",
+//       repetition: Repetition[0],
+//       startTime: TimeOfDay.now().toString(),
+//       endTime: TimeOfDay.now().toString(),
+//       deadline: DateTime.now().toString(),
+//       isComplete: true,
+//       isFavorite: false),
+//   Task(
+//       taskLabel: "this is task 3",
+//       repetition: Repetition[0],
+//       startTime: TimeOfDay.now().toString(),
+//       endTime: TimeOfDay.now().toString(),
+//       deadline: DateTime.now().toString(),
+//       isComplete: false,
+//       isFavorite: false),
+// ];
 
 class AllTasks extends StatefulWidget {
   const AllTasks({Key? key}) : super(key: key);
@@ -44,15 +44,32 @@ class _AllTasksState extends State<AllTasks> {
   Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height * 0.75,
-      child: ListView.builder(
-          //to be assigned to fetched list.size
-          itemCount: tasks.length,
-          itemBuilder: (BuildContext context, int index) {
-            return TaskListTile(
-                taskLabel: tasks[index].taskLabel,
-                isComplete: tasks[index].isComplete,
-                isFavorite: tasks[index].isFavorite);
-          }),
+      child: FutureBuilder<List<Task>>(
+        future: TaskDatabase.instance.getAllTasks(),
+        builder: (context, future) {
+          if (!future.hasData) {
+            //has no data
+            return Center(
+              child: Text("You haven't created tasks yet.",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).primaryColor)),
+            );
+          } else {
+            List<Task> tasks = future.data!;
+            return ListView.builder(
+                //to be assigned to fetched list.size
+                itemCount: tasks.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return TaskListTile(
+                      taskLabel: tasks[index].taskLabel,
+                      isComplete: tasks[index].isComplete,
+                      isFavorite: tasks[index].isFavorite);
+                });
+          }
+        },
+      ),
     );
   }
 }
