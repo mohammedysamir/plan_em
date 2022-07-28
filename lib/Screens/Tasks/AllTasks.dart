c73f4daabfeda7088d74be637eb042e94eb6ad50
Mohammed Yasser <mohammedre4a@gmail.com>
2022-07-28 03:26:03 +0200

    feat: tried to fetch data from db, need to be tested and enhanced

--- lib/Screens/Tasks/AllTasks.dart
+++ lib/Screens/Tasks/AllTasks.dart
@@ -1,36 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:plan_em/TaskDatabase.dart';
 import 'package:plan_em/reusable_components/TaskListTile.dart';
-
 import '../../Data models/Task.dart';
-
-//todo: create list of listTiles to be viewed and fetched from DB
-// List<Task> tasks = <Task>[
-//   Task(
-//       taskLabel: "this is task 2",
-//       repetition: Repetition[1],
-//       startTime: TimeOfDay.now().toString(),
-//       endTime: TimeOfDay.now().toString(),
-//       deadline: DateTime.now().toString(),
-//       isComplete: false,
-//       isFavorite: true),
-//   Task(
-//       taskLabel: "this is task 1",
-//       repetition: Repetition[0],
-//       startTime: TimeOfDay.now().toString(),
-//       endTime: TimeOfDay.now().toString(),
-//       deadline: DateTime.now().toString(),
-//       isComplete: true,
-//       isFavorite: false),
-//   Task(
-//       taskLabel: "this is task 3",
-//       repetition: Repetition[0],
-//       startTime: TimeOfDay.now().toString(),
-//       endTime: TimeOfDay.now().toString(),
-//       deadline: DateTime.now().toString(),
-//       isComplete: false,
-//       isFavorite: false),
-// ];
 
 class AllTasks extends StatefulWidget {
   const AllTasks({Key? key}) : super(key: key);
@@ -40,17 +11,21 @@ class AllTasks extends StatefulWidget {
 }
 
 class _AllTasksState extends State<AllTasks> {
+  TaskDatabase databaseHandler=TaskDatabase();
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height * 0.75,
       child: FutureBuilder<List<Task>>(
-        future: TaskDatabase.instance.getAllTasks(),
+        future:
+        databaseHandler.getAllTasks(),
         builder: (context, future) {
-          if (!future.hasData) {
+          if (future.data != null) {
             //has no data
             return Center(
               child: Text("You haven't created tasks yet.",
+                  textAlign: TextAlign.center,
                   style: TextStyle(
                       fontSize: 22,
                       fontWeight: FontWeight.bold,
